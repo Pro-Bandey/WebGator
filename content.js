48a9e99d636289e8f9aaa94841f70222d51e02fd
Pro Bandey <195909043+Pro-Bandey@users.noreply.github.com>
2025-12-18 23:31:57 +0500

    ok

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 (() => {
   const TOP_TRIGGER_HEIGHT = 60;
-  const CENTER_TRIGGER_WIDTH = 180;
+  const CENTER_TRIGGER_WIDTH = 120;
   const HIDE_DELAY = 200;
   const START_PAGE = "https://homepage-gamma-dun.vercel.app/";
 
